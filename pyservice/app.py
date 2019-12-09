a766d3eb5d1015e4fffbcf49b88c8b0be4bf68a5
tuns <tuns@vng.com.vn>
2019-12-09 16:44:27 +0700

    fix package structure

--- pyservice/app.py
+++ pyservice/app.py
@@ -1,3 +1,27 @@
-from .app.main import main
+# Created by tuns at 2019-12-09 16:42:22
+# Email tuns@vng.com.vn
 
-__all__ = ['main']
+import functools
+from .utils import before_run
+from .utils import get_args_parser
+from .utils import set_app_args
+
+def main(app_name):
+
+    def main_decorator(some_func):
+        @functools.wraps(some_func)
+        def wrapper(*args, **kwargs):
+            cmd_args = get_args_parser()
+            try:
+                before_run(app_name, cmd_args)
+                set_app_args(cmd_args)
+                result = some_func(*args, **kwargs)
+                return result
+            except KeyboardInterrupt:
+                import sys
+                sys.exit(-1)
+            except SystemExit:
+                pass
+        return wrapper
+
+    return main_decorator
--- pyservice/app/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import utils
-from .main import main
-
-__all__ = ['main']
